76c54377f8434c9fc6d1a54fd94d1b3c05fb201c
Proba <teplyakovateplyakova94@gmail.com>
2024-10-20 16:40:21 +1000

    Добавлена метка с названием базовой валюты

--- Obmen.py
+++ Obmen.py
@@ -23,13 +23,19 @@ def exchange():
         except Exception as e:
             mb.showerror("Ошибка", f"Произошла ошибка: {e}.")
     else:
-        mb.showwarning("Внимание!", "Введите код валюты")
+        mb.showwarning("Внимание!", "Выберите код валюты")
 
 
-def update_c_label(event):
+def update_b_label(event):
+    code = b_combobox.get()
+    name = cur[code]
+    b_label.config(text=name)
+
+
+def update_t_label(event):
     code = t_combobox.get()
     name = cur[code]
-    c_label.config(text=name)
+    t_label.config(text=name)
 
 cur = {
     'RUB': 'Российский рубль',
@@ -53,15 +59,18 @@ Label(text="Базовая валюта").pack(padx=10, pady=10)
 
 b_combobox = ttk.Combobox(values=list(cur.keys()))
 b_combobox.pack(padx=10, pady=10)
+b_combobox.bind("<<ComboboxSelected>>", update_b_label)
+b_label = ttk.Label()
+b_label.pack(padx=10, pady=10)
 
 Label(text="Целевая валюта").pack(padx=10, pady=10)
 
 t_combobox = ttk.Combobox(values=list(cur.keys()))
 t_combobox.pack(padx=10, pady=10)
-t_combobox.bind("<<ComboboxSelected>>", update_c_label)
+t_combobox.bind("<<ComboboxSelected>>", update_t_label)
 
-c_label = ttk.Label()
-c_label.pack(padx=10, pady=10)
+t_label = ttk.Label()
+t_label.pack(padx=10, pady=10)
 
 Button(text="Получить курс обмена", command = exchange).pack(padx=10, pady=10)
 
