c7ed8919681e6d6568c282b27b41a1c2427ab818
Proba <teplyakovateplyakova94@gmail.com>
2024-10-20 15:37:07 +1000

    Вместо поля ввода добавлен выпадающий список

--- Obmen.py
+++ Obmen.py
@@ -2,10 +2,11 @@ import requests
 import json
 from tkinter import *
 from tkinter import messagebox as mb
+from tkinter import ttk
 
 
 def exchange():
-    code = entry.get()
+    code = combobox.get()
     if code:
         try:
             response = requests.get("https://open.er-api.com/v6/latest/USD")
@@ -28,10 +29,14 @@ window = Tk()
 window.title("Курсы обмена валют")
 window.geometry("360x180")
 
-Label(text="Введите код валюты").pack(padx=10, pady=10)
+Label(text="Выберите код валюты").pack(padx=10, pady=10)
+cur = ['RUB', 'EUR', 'GBP', 'JPY', 'CNY', 'KZT', 'UZS', 'CHF', 'AED', 'CAD']
+combobox = ttk.Combobox(values=cur)
+combobox.pack(padx=10, pady=10)
 
-entry = Entry()
-entry.pack(padx=10, pady=10)
+# entry = Entry()
+# entry.pack(padx=10, pady=10)
+
 Button(text="Получить курс обмена к доллару", command = exchange).pack(padx=10, pady=10)
 
 window.mainloop()
